c7164eb36ba51a7f69e3f3711d2cec672d1e1653
matthimily12 <151691666+matthimily12@users.noreply.github.com>
2026-05-22 11:56:13 -0600

    Update mipagina.php

--- paginas/mipagina.php
+++ paginas/mipagina.php
@@ -1,10 +1,10 @@
 ﻿<?php
 
 $conexion = mysqli_connect(
-    "aws-123.mysql.server.com",
-    "usuario123",
-    "contraseña123",
-    "ephoria"
+    "host_online",
+    "usuario",
+    "password",
+    "nombre_bd"
 );
 
 if(!$conexion){
